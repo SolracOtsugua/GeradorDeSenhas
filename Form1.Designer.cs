51c33aacb670f4c858b908798bd100692fa012c2
Carlos Augusto <carlosaugustoslima@gmail.com>
2026-04-06 15:42:01 -0300

    Commit inicial do projeto GeradorDeSenhas

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace GeradorDeSenhas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numComprimento = new System.Windows.Forms.NumericUpDown();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnGerar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numComprimento)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(207, 204);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Comprimento";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // numComprimento
+            // 
+            this.numComprimento.Location = new System.Drawing.Point(210, 231);
+            this.numComprimento.Name = "numComprimento";
+            this.numComprimento.Size = new System.Drawing.Size(120, 20);
+            this.numComprimento.TabIndex = 1;
+            this.numComprimento.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(397, 231);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.ReadOnly = true;
+            this.txtSenha.Size = new System.Drawing.Size(100, 20);
+            this.txtSenha.TabIndex = 2;
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.Location = new System.Drawing.Point(210, 321);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(75, 23);
+            this.btnGerar.TabIndex = 3;
+            this.btnGerar.Text = "Gerar Senha";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGerar);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.numComprimento);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numComprimento)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numComprimento;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnGerar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text; //Necessário para usar o StringBuilder
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeradorDeSenhas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            //1. Definir os caracteres possíveis
+            string caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*";
+
+            //2. Pegar o comprimento escolhido no NumericUpDown
+            int comprimento = (int)numComprimento.Value;
+
+            //3. Gerador de números aleatórios
+            Random random = new Random();
+            StringBuilder senha = new StringBuilder();
+
+            //4. Sortear caracteres
+            for (int i = 0; i < comprimento; i++)
+            {
+                int indice = random.Next(caracteres.Length);
+                senha.Append(caracteres[indice]);
+            }
+
+            //5. Exibir no TextBox
+            txtSenha.Text = senha.ToString();
+        }
+    }
+}
